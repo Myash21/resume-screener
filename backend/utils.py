df6ef33ee8447d0738e055c0e7120580b4171c18
Piyush Choudhari <choudhari.piyush@gmail.com>
2024-09-27 11:54:24 +0530

    modified utils.py

--- backend/utils.py
+++ backend/utils.py
@@ -1,76 +1,25 @@
 import os
 import logging
-import re
 from typing import List, Tuple
 from langchain_chroma import Chroma
 from langchain.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 from dotenv import load_dotenv
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.probability import FreqDist
-from nltk import pos_tag
+
 
 load_dotenv()
 
 
-def extract_keywords(prompt: str) -> str:
-    nltk.download("punkt", quiet=True)
-    nltk.download("punkt_tab", quiet=True)
-    nltk.download("stopwords", quiet=True)
-    nltk.download("averaged_perceptron_tagger", quiet=True)
-    nltk.download("averaged_perceptron_tagger_eng", quiet=True)
-
-    tokens = word_tokenize(prompt.lower())
-
-    stop_words = set(stopwords.words("english"))
-    filtered_tokens = [
-        word for word in tokens if word.isalnum() and word not in stop_words
-    ]
-
-    tagged_tokens = pos_tag(filtered_tokens)
-
-    keywords = [
-        word for word, tag in tagged_tokens if tag in ("NN", "NNS", "JJ", "JJR", "JJS")
-    ]
-    fdist = FreqDist(keywords)
-
-    keywords = fdist.most_common()
-    keyword_joint = " ".join([keyword for keyword, idx in keywords]).title()
-
-    return keyword_joint
-
-
-def extract_urls(url_list: List[str]) -> List[str]:
-    url_pattern = re.compile(
-        r"http[s]?://"
-        r"(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|"
-        r"(?:%[0-9a-fA-F][0-9a-fA-F]))+"
-    )
-
-    extracted_urls = []
-    for text in url_list:
-        match = url_pattern.search(text)
-        if match:
-            extracted_urls.append(match.group(0))
-
-    return extracted_urls
-
-
-def query_rag(query_text: str) -> Tuple[str, List[str]] | str:
-    question_content = query_text.split("|>")
-    question, language = question_content[0], question_content[-1]
-
+def query_rag(job_description: str) -> Tuple[str, List[str]] | str:
     PROMPT_TEMPLATE = """
-    Here is the context provided:
+    Here is the job description provided:
 
-    {context}
+    {job_description}
 
     ---
 
-    Answer the following question based on the above context. If the question is a greeting, farewell, or expression of thanks, respond warmly and personally without referencing the context. For queries unrelated to legal content, reply with: "I’m sorry, but I can’t assist with that." Please ensure your response is descriptive and informative based on the context. Answer in {language}.
+    Answer the following question based on the above context. If the question is a greeting, farewell, or expression of thanks, respond warmly and personally without referencing the context. For queries unrelated to legal content, reply with: "I’m sorry, but I can’t assist with that." Please ensure your response is descriptive and informative based on the context.
 
     Question: {question}
     """
@@ -88,19 +37,13 @@ def query_rag(query_text: str) -> Tuple[str, List[str]] | str:
             ),
         )
 
-        results = db.similarity_search_with_score(query_text, k=5)
-        hyperlink_results = db_links.similarity_search_with_score(
-            extract_keywords(query_text), k=5
-        )
+        results = db.similarity_search_with_score(job_description, k=5)
+
 
         context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
-        context_links = extract_urls(
-            [doc.page_content for doc, _score in hyperlink_results]
-        )
-        print(context_links)
         prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
         prompt = prompt_template.format(
-            context=context_text, question=question, language=language
+            context=context_text, question=question
         )
 
         model = ChatGoogleGenerativeAI(
@@ -109,7 +52,7 @@ def query_rag(query_text: str) -> Tuple[str, List[str]] | str:
         response = model.invoke(prompt)
 
         if hasattr(response, "content"):
-            return response.content.strip(), context_links
+            return response.content.strip()
         return str(response).strip(), context_links
 
     except Exception as e:
